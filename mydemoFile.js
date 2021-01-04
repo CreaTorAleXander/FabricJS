7aa80e999ff5de5bb006ca78d9e0b53e8d4c219f
alexanderhess <Hess.alexander1@web.de>
2021-01-04 12:10:59 +0100

    added the image to the canvas

--- mydemoFile.js
+++ mydemoFile.js
@@ -1,14 +1,24 @@
-let pic = document.getElementById("demo-pic");
+
+var canvas = new fabric.Canvas('c');
+
+let imgElement = document.getElementById("demo-pic");
+var imgInstance = new fabric.Image(imgElement, {
+  left: 100,
+  top: 100,
+  angle: 30,
+  opacity: 0.85
+})
+canvas.add(imgInstance);
 
 document.body.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
-    pic.setAttribute(
+    imgElement.setAttribute(
       "style",
       "-webkit-filter: drop-shadow(1px 1px 0 black)drop-shadow(-1px -1px 0 black);filter: drop-shadow(1px 1px 0 black) drop-shadow(-1px -1px 0 black);"
     );
     document.body.style.backgroundColor = "lightcoral";
   } else if (e.target.tagName === "BODY") {
-    pic.setAttribute("style", "-webkit-filter: none;");
+    imgElement.setAttribute("style", "-webkit-filter: none;");
     document.body.style.backgroundColor = "white";
   }
 });
