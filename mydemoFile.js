bd3be84846a2aa620b615bd403c9ee2195734877
alexanderhess <Hess.alexander1@web.de>
2021-01-04 11:47:42 +0100

    toggle img clicked body clicked

--- mydemoFile.js
+++ mydemoFile.js
@@ -1,6 +1,14 @@
 let pic = document.getElementById("demo-pic");
-console.log(pic);
-pic.addEventListener("click", () => {
-  
-    pic.setAttribute("style", "-webkit-filter: none ")
-})
+
+document.body.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    pic.setAttribute(
+      "style",
+      "-webkit-filter: drop-shadow(1px 1px 0 black)drop-shadow(-1px -1px 0 black);filter: drop-shadow(1px 1px 0 black) drop-shadow(-1px -1px 0 black);"
+    );
+    document.body.style.backgroundColor = "lightcoral";
+  } else if (e.target.tagName === "BODY") {
+    pic.setAttribute("style", "-webkit-filter: none;");
+    document.body.style.backgroundColor = "white";
+  }
+});
